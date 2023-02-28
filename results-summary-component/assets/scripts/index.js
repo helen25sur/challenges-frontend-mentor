cd9db45de6c8627350a3cd7b978b8dec31169365
Elena Surilova <elen21surilova@gmail.com>
2023-02-28 20:08:00 +0200

    9: add getting data from data.json

---
+++ results-summary-component/assets/scripts/index.js
@@ -0,0 +1,18 @@
+const userScore = document.body.querySelector('.user-score>strong');
+
+const summaryItemsList = document.body.querySelectorAll('.summary-item');
+
+fetch('../../data.json')
+  .then((response) => response.json())
+  .then((data) => {
+    let score = 0;
+    summaryItemsList.forEach((item, idx) => {
+      item.querySelector('.item-title').innerText = data[idx].category;
+      item.querySelector('.item-score>strong').innerText = data[idx].score;
+      score += data[idx].score;
+    });
+    const average = Math.round(score / data.length);
+
+    userScore.innerText = average;
+  }
+  );
